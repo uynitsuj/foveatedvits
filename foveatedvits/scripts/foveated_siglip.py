231a48a800356a167130a914f92dca228a163158
Justin Yu <yujustin8@gmail.com>
2025-08-06 11:26:14 -0700

    foveated siglip progress

--- foveatedvits/scripts/foveated_siglip.py
+++ foveatedvits/scripts/foveated_siglip.py
@@ -6,8 +6,10 @@ import jax.numpy as jnp
 import jax
 import imageio
 from foveatedvits.utils.siglip_loader import load_siglip_so400m224
-
+import time
 # "SigLIP So400m/14 224": "gs://big_vision/siglip/webli_en_so400m_224_57633886.npz:img",
+
+jax.config.update("jax_platform_name", "gpu")
 
 # img = nnx_bridge.ToNNX(
 model = _siglip.Module(
@@ -24,7 +26,11 @@ variables = model.lazy_init(rng, jnp.ones((1, 224, 224, 3)), train=False)
 params = variables["params"]
 
 params = load_siglip_so400m224(params)
-variables = {"params": params}
+# variables = {"params": params}
+
+@jax.jit
+def forward(p, x):                     # compiled once
+    return model.apply({'params': p}, x, train=False)
 
 # Load video from path
 video_path = "/home/justinyu/foveatedvits/foveatedvits/scripts/example_media/episode_000000.mp4"
@@ -32,8 +38,9 @@ video = imageio.get_reader(video_path)
 
 # Embed video frames
 for frame in video:
-    frame = jax.image.resize(
-        jnp.asarray(frame, jnp.float32) / 255.0, (224, 224, 3), "bilinear"
-    )
-    emb, _ = model.apply(variables, frame[None], train=False)
+    
+    start_time = time.time()
+    emb, _ = forward(params, frame[None])
+    end_time = time.time()
+    print(f"Time taken for SigLIP forward pass: {end_time - start_time} seconds")
     import pdb; pdb.set_trace()
